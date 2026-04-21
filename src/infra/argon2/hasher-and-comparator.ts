90e9268bac4c9e37920d4858e41abea3e51c99a4
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-20 23:16:17 -0300

    feat: add `Argon2HasherAndComparator` implementing both `Hasher` & `HashComparator` interfaces

---
+++ src/infra/argon2/hasher-and-comparator.ts
@@ -0,0 +1,25 @@
+/**
+ * TODO: allow `parallelism`, `memoryCost` and `timeCost` to be
+ * defined through environment variables.
+ *
+ * See: https://github.com/ranisalt/node-argon2/wiki/Options
+ */
+
+import { Injectable } from "@nestjs/common";
+import * as argon2 from "argon2";
+import { HashComparator } from "src/modules/crypto/comparator";
+import { Hasher } from "src/modules/crypto/hasher";
+
+@Injectable()
+export class Argon2HasherAndComparator implements Hasher, HashComparator {
+  public async compare(plainText: string, hash: string): Promise<boolean> {
+    return await argon2.verify(hash, plainText);
+  }
+
+  public async hash(plainText: string): Promise<string> {
+    return await argon2.hash(plainText, {
+      memoryCost: 0x4000, // 16MiB
+      parallelism: 2,
+    });
+  }
+}
