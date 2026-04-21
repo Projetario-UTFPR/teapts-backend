8d01e5d11588b10c9133c51edac6a617ac7169ee
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-20 22:48:15 -0300

    feat: add `AuthenticateAccountService`

---
+++ src/modules/identity/services/authenticate-account.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@nestjs/common";
+import { HashComparator } from "src/modules/crypto/comparator";
+import { AccountsRepository } from "../repositories/accounts.repository";
+import { pipe } from "fp-ts/lib/function";
+import { either as e, taskEither as te } from "fp-ts";
+import { InvalidCredentialsError } from "../errors/invalid-credentials.error";
+import { ResourceNotFoundError } from "src/common/errors/resource-not-found.error";
+import { Account } from "../entities/account";
+
+type Params = {
+  email: string;
+  plainPassword: string;
+};
+
+@Injectable()
+export class AuthenticateAccountService {
+  public constructor(
+    private readonly comparator: HashComparator,
+    private readonly accountsRepo: AccountsRepository,
+  ) {}
+
+  public async execute({ email, plainPassword }: Params) {
+    return pipe(
+      this.findAccount(email),
+      te.chainFirstW((account) => this.verifyPassword(account, plainPassword)),
+    );
+  }
+
+  private findAccount(email: string) {
+    return pipe(
+      () => this.accountsRepo.findAccountByEmail(email),
+      te.mapLeft((error) => {
+        return error instanceof ResourceNotFoundError ? new InvalidCredentialsError() : error;
+      }),
+    );
+  }
+
+  private verifyPassword(account: Account, plainPassword: string) {
+    return pipe(
+      te.fromTask(() => this.comparator.compare(plainPassword, account.getPasswordHash())),
+      te.chainEitherK((credentialsMatch) => {
+        return credentialsMatch ? e.right(undefined) : e.left(new InvalidCredentialsError());
+      }),
+    );
+  }
+}
