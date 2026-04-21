6e72eb8ccf14ae6778d02fa57c1e80feeb95d04e
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-20 22:44:37 -0300

    feat: add `AccountsRepository` interface with `findAccountByEmail` method

---
+++ src/modules/identity/errors/account-not-found.error.ts
@@ -0,0 +1,11 @@
+import { ResourceNotFoundError } from "src/common/errors/resource-not-found.error";
+import { Account } from "../entities/account";
+
+export class AccountNotFoundError extends ResourceNotFoundError {
+  public constructor() {
+    super({
+      message: "Não foi possível encontrar nenhuma conta com essas credenciais.",
+      subject: Account.name,
+    });
+  }
+}
