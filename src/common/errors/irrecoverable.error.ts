ed3e24b0bdec34432c631817120177c28fac51ba
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-24 16:20:56 -0300

    chore: rename `IIrrecoverableError` interface

--- src/common/errors/irrecoverable.error.ts
+++ src/common/errors/irrecoverable.error.ts
@@ -1,6 +1,6 @@
 import { BaseError } from "./base.error";
 
-interface IIRrecoverableError {
+interface IIrrecoverableError {
   /**
    * A developer-targeted message describing what possibly triggered this error.
    */
@@ -15,14 +15,14 @@ interface IIRrecoverableError {
  * A error that is not possible to be treated by the client user because it either did not
  * originate in the server or it is a programming-level error that must be fixed by a developer.
  */
-export class IrrecoverableError extends BaseError implements IIRrecoverableError {
+export class IrrecoverableError extends BaseError implements IIrrecoverableError {
   public readonly message: string;
 
   public get cause() {
-    return this._cause as IIRrecoverableError["cause"];
+    return this._cause as IIrrecoverableError["cause"];
   }
 
-  public constructor({ cause, message }: IIRrecoverableError) {
+  public constructor({ cause, message }: IIrrecoverableError) {
     super({ cause });
     this.message = message;
   }
