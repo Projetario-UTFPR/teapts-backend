9aba13d33bc9c903ffcab27595d278b84e91368c
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-25 14:26:47 -0300

    chore(`AccountsRepository`): introduce `findAccountById` method

--- src/infra/prisma/repositories/accounts.repository.ts
+++ src/infra/prisma/repositories/accounts.repository.ts
@@ -1,11 +1,12 @@
 import { IrrecoverableError } from "@/common/errors/irrecoverable.error";
+import { either as e, taskEither as te } from "fp-ts";
+import { UUID } from "@/common/uuid";
 import { PrismaService } from "@/infra/prisma/prisma";
 import { Account } from "@/modules/identity/entities/account.aggregate";
 import { AccountNotFoundError } from "@/modules/identity/errors/account-not-found.error";
 import { AccountsRepository } from "@/modules/identity/repositories/accounts.repository";
 import { Injectable } from "@nestjs/common";
-import { either as e, taskEither as te } from "fp-ts";
-import { Either } from "fp-ts/lib/Either";
+import { Prisma } from "@prisma-gen/browser";
 import { pipe } from "fp-ts/lib/function";
 
 @Injectable()
@@ -14,22 +15,35 @@ export class PrismaAccountsRepository extends AccountsRepository {
     super();
   }
 
-  public findAccountByEmail(
-    email: string,
-  ): Promise<Either<IrrecoverableError | AccountNotFoundError, Account>> {
+  public findAccountByEmail(email: string) {
+    return this.findAccountWithClause({ email });
+  }
+
+  public findAccountById(id: UUID) {
+    return this.findAccountWithClause({ id: id.toString() });
+  }
+
+  private findAccountWithClause(where: Prisma.AccountWhereUniqueInput) {
     return pipe(
       te.tryCatch(
-        () => this.prisma.account.findUnique({ where: { email } }),
+        () =>
+          this.prisma.account.findUnique({
+            include: { professionalProfiles: { select: { id: true } } },
+            where,
+          }),
         (error) =>
           new IrrecoverableError({
-            message: `Error occurred in ${PrismaAccountsRepository.name} when trying to find account by email '${email}'.`,
+            message: `Error occurred in ${PrismaAccountsRepository.name} when trying to find account with where clause '${JSON.stringify(where)}'.`,
             cause: error as Error,
           }),
       ),
       te.chainEitherKW((rawAccount) => {
         if (rawAccount === null) return e.left(new AccountNotFoundError());
 
-        const account = Account.create(rawAccount);
+        const { professionalProfiles, ...rawAccountPayload } = rawAccount;
+        const professionalProfilesIds = professionalProfiles.map((profile) => profile.id);
+        const account = Account.create({ ...rawAccountPayload, professionalProfilesIds });
+
         return e.right(account);
       }),
     )();
--- src/modules/identity/repositories/accounts.repository.ts
+++ src/modules/identity/repositories/accounts.repository.ts
@@ -2,9 +2,14 @@ import { Either } from "fp-ts/lib/Either";
 import { AccountNotFoundError } from "../errors/account-not-found.error";
 import { IrrecoverableError } from "@/common/errors/irrecoverable.error";
 import { Account } from "@/modules/identity/entities/account.aggregate";
+import { type UUID } from "@/common/uuid";
 
 export abstract class AccountsRepository {
   public abstract findAccountByEmail(
     email: string,
+  ): Promise<Either<IrrecoverableError | AccountNotFoundError, Account>>;
+
+  public abstract findAccountById(
+    id: UUID,
   ): Promise<Either<IrrecoverableError | AccountNotFoundError, Account>>;
 }
