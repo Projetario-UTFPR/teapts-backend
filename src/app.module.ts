04af8077817d2ab3eb70f3ea2679f9b435d7d50e
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-24 22:16:42 -0300

    chore: register `ExceptionsModule` and `ValidationProviderModule` in `AppModule`

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { Argon2Module } from "./infra/argon2/argon2.module";
+import { ExceptionsModule } from "@/infra/http/exceptions/exceptions.module";
+import { ValidationProviderModule } from "@/infra/http/validation-provider/validation-provider.module";
 
 @Module({
-  imports: [Argon2Module],
+  imports: [Argon2Module, ExceptionsModule, ValidationProviderModule],
   controllers: [AppController],
   providers: [AppService],
 })
