4f7dbb72d9efba4c2f47db982f5e94fca81992c8
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-24 23:20:52 -0300

    change(`DTO`): transforma a interface em uma classe abstrata com implementação padrão do método `validate`

--- src/infra/http/dto.ts
+++ src/infra/http/dto.ts
@@ -1,9 +1,23 @@
 import { ValidationErrorsBag } from "@/common/errors/validation-errors-bag.error";
+import { mapZodErrorsToValidationErrors } from "@/lib/zod";
+import { either as e } from "fp-ts";
 import { Either } from "fp-ts/lib/Either";
+import { ZodType } from "zod";
 
-export interface DTO {
+export abstract class DTO {
+  protected abstract readonly schema: ZodType;
+
   /**
    * Validates this `DTO` instance, ensuring it's a valid payload.
    */
-  validate(): Either<ValidationErrorsBag, void>;
+  validate(): Either<ValidationErrorsBag, void> {
+    const validationResult = this.schema.safeParse(this);
+
+    if (!validationResult.success) {
+      return e.left(mapZodErrorsToValidationErrors(validationResult.error));
+    }
+
+    Object.assign(this, validationResult.data);
+    return e.right(undefined);
+  }
 }
