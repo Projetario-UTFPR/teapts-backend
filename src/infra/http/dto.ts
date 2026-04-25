b9e4cc0ebd21388547c27c6e80cb5dc6a094b81a
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-24 22:00:52 -0300

    chore: add `DTO` interface

---
+++ src/infra/http/dto.ts
@@ -0,0 +1,9 @@
+import { ValidationErrorsBag } from "@/infra/http/validation/validation-errors-bag.error";
+import { Either } from "fp-ts/lib/Either";
+
+export interface DTO {
+  /**
+   * Validates this `DTO` instance, ensuring it's a valid payload.
+   */
+  validate(): Either<ValidationErrorsBag, void>;
+}
