e17470fc25ec533115c68ca87ef170147a5d3ca2
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-01 18:56:38 -0300

    build: introduce new optional environment variable `APP_VERSION`

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -4,6 +4,7 @@ import z from "zod";
 const schema = z.object({
   APP_NAME: z.string("APP_NAME is a required environment variable and must be a string."),
   APP_URL: z.url("URL must be a valid URL string."),
+  APP_VERSION: z.string().optional(),
 });
 
 export default registerAs("app", () => schema.parse(process.env));
