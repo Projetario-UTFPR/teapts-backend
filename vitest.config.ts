ce6632862e32e692e1d8fea81c1a114dc5820146
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-01 04:43:06 -0300

    test: config environment variables for tests environments

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from "vitest/config";
+import { configDotenv } from "dotenv";
+import { expand } from "dotenv-expand";
+
+expand(configDotenv());
 
 export default defineConfig({
   test: {
