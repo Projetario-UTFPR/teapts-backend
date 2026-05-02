bc2a061f31371bae773559294d67d13ebe14e9c8
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-01 21:04:37 -0300

    chore(`main.ts`): replace `process.env.PORT` with `appConfigs.PORT`

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -5,7 +5,7 @@ const schema = z.object({
   APP_NAME: z.string("APP_NAME is a required environment variable and must be a string."),
   APP_URL: z.url("URL must be a valid URL string."),
   APP_VERSION: z.string().optional(),
-  PORT: z.number().default(3000),
+  PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(["development", "production"]).default("development"),
 });
 
