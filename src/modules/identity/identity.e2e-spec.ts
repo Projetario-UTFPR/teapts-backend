10a006eeab4d1623b0d14686e0ab628ca1e9ff24
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-03 17:07:04 -0300

    test(`IdentityController`): get tests passing

--- src/modules/identity/identity.e2e-spec.ts
+++ src/modules/identity/identity.e2e-spec.ts
@@ -1,4 +1,6 @@
 import { AppModule } from "@/app.module";
+import { SignUpDto } from "@/modules/identity/dtos/signUp.dto";
+import { faker } from "@faker-js/faker";
 import { type INestApplication } from "@nestjs/common";
 import { Test, type TestingModule } from "@nestjs/testing";
 import supertest from "supertest";
@@ -8,7 +10,7 @@ import type { App } from "supertest/types";
 describe("[e2e] Identity Controller (v1)", () => {
   let app: INestApplication<App>;
 
-  const name = "FoooooBar";
+  const name = faker.person.fullName();
   const email = "existingandknown@email.com";
   const plainPassword = "12345678";
 
@@ -21,45 +23,64 @@ describe("[e2e] Identity Controller (v1)", () => {
     await app.init();
   });
 
+  const assertIsValidationErrorsBag = (response: supertest.Response) => {
+    expect(response.body, "response should be a validation bag object").toHaveProperty("errors");
+  };
+
   test("create account route is public", async () => {
     const response = await request(app.getHttpServer()).post("/v1/identities/create-account");
     expect(response.status, "A rota não deveria ser privada.").not.toBe(401);
   });
 
   it("should refuse to register an user with incorrect email syntax", async () => {
-    const response = await supertest(app.getHttpServer())
+    const failureResponse = await supertest(app.getHttpServer())
       .post("/v1/identities/create-account")
-      .send({ name, email: "existingandknownemail.com", plainPassword })
+      .send({
+        email: "existingandknownemail.com",
+        password: plainPassword,
+        name,
+      } satisfies SignUpDto.Type)
       .expect(422);
 
-    console.log(response.body);
-    await supertest(app.getHttpServer())
-      .post("/v1/identities/create-account")
-      .send({ name, email, plainPassword })
-      .expect(200);
+    assertIsValidationErrorsBag(failureResponse);
+    expect(
+      failureResponse.body.errors,
+      "response should contain email validation errors",
+    ).toHaveProperty("email");
   });
 
   it("should refuse to register an user with incorrect password length", async () => {
-    await supertest(app.getHttpServer())
+    const failureRes = await supertest(app.getHttpServer())
       .post("/v1/identities/create-account")
-      .send({ name, email, plainPassword: "1" })
-      .expect(401);
+      .send({ name, email, password: "1" })
+      .expect(422);
 
-    await supertest(app.getHttpServer())
-      .post("/v1/identities/create-account")
-      .send({ name, email, plainPassword })
-      .expect(200);
+    assertIsValidationErrorsBag(failureRes);
+    expect(
+      failureRes.body.errors,
+      "response should contain password validation errors",
+    ).toHaveProperty("password");
   });
 
   it("should refuse to register an user with incorrect user name length ", async () => {
-    await supertest(app.getHttpServer())
+    const failureResponse = await supertest(app.getHttpServer())
       .post("/v1/identities/create-account")
-      .send({ name: "La", email, plainPassword })
-      .expect(401);
+      .send({ name: "La", email, password: plainPassword } satisfies SignUpDto.Type)
+      .expect(422);
 
-    await supertest(app.getHttpServer())
+    assertIsValidationErrorsBag(failureResponse);
+    expect(
+      failureResponse.body.errors,
+      "response should contain name validation errors",
+    ).toHaveProperty("name");
+  });
+
+  it("should create an account successfully", async () => {
+    const response = await supertest(app.getHttpServer())
       .post("/v1/identities/create-account")
-      .send({ name, email, plainPassword })
-      .expect(200);
+      .send({ name, email, password: plainPassword } satisfies SignUpDto.Type)
+      .expect(204);
+
+    console.log(response.body);
   });
 });
