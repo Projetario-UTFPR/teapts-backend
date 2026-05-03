dd4ba25e6b7747a59eb2574302c6aac3d56741b3
Marco Antônio <164404924+Marco4325@users.noreply.github.com>
2026-05-03 19:46:17 +0000

    change: transfered validations from create-account service to DTO

--- src/modules/identity/services/create-account.service.ts
+++ src/modules/identity/services/create-account.service.ts
@@ -4,14 +4,11 @@ import { AccountsRepository } from "../repositories/accounts.repository";
 import { Account } from "../entities/account.aggregate";
 
 import { Hasher } from "../../crypto/hasher";
-import { either as e } from "fp-ts";
-import { InvalidCredentialsError } from "@/modules/identity/errors/invalid-credentials.error";
 
 type TCreateAccount = {
   name: string;
   email: string;
   plainPassword: string;
-  professionalProfilesIds: string[];
 };
 
 @Injectable()
@@ -24,41 +21,9 @@ export class CreateAccountService {
   public async execute(params: TCreateAccount) {
     const { plainPassword, ...accountProps } = params;
 
-    const validation = await this.verifyCredentials(params);
-
-    if (e.isLeft(validation)) {
-      return validation;
-    }
-
     const passwordHash = await this.hasher.hash(plainPassword);
-    const account = Account.create({ ...accountProps, passwordHash });
+    const account = Account.create({ ...accountProps, passwordHash, professionalProfilesIds: [] });
 
-    const result = await this.accountsRepo.create(account);
-    return e.right(result);
-  }
-
-  private async verifyCredentials({ email, name, plainPassword }) {
-    const emailValidation = this.verifyEmailSyntax(email);
-    if (emailValidation) return emailValidation;
-
-    const nameValidation = this.verifyUserNameSyntax(name);
-    if (nameValidation) return nameValidation;
-
-    const passwordValidation = this.verifyPasswordSyntax(plainPassword);
-    if (passwordValidation) return passwordValidation;
-
-    return e.right(undefined);
-  }
-
-  private verifyEmailSyntax(email: string) {
-    return !email.includes("@") ? e.left(new InvalidCredentialsError()) : null;
-  }
-
-  private verifyUserNameSyntax(name: string) {
-    return name.length < 5 ? e.left(new InvalidCredentialsError()) : null;
-  }
-
-  private verifyPasswordSyntax(password: string) {
-    return password.length < 8 ? e.left(new InvalidCredentialsError()) : null;
+    return await this.accountsRepo.create(account);
   }
 }
