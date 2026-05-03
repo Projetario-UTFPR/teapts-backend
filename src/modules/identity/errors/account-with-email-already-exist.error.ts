beca6d729b95a2331113cfe8d0a6b21a06141b4b
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-03 17:20:32 -0300

    feat: introduce `ConflictError` & makes `AccountWithEmailAlreadyExistError` inherit it

--- src/modules/identity/errors/account-with-email-already-exist.error.ts
+++ src/modules/identity/errors/account-with-email-already-exist.error.ts
@@ -1,6 +1,6 @@
-import { BadRequestError } from "@/common/errors/bad-request.error";
+import { ConflictError } from "@/common/errors/conflict.error";
 
-export class AccountWithEmailAlreadyExistError extends BadRequestError {
+export class AccountWithEmailAlreadyExistError extends ConflictError {
   public constructor() {
     super({ message: "Já existe uma conta associada a este email." });
   }
