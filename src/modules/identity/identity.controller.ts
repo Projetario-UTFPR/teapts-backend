bb955749a6465fb54bcb670115c70ba2c9b0112a
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-03 17:28:34 -0300

    docs(`IdentityController`): replace bad request response with unprocessable entity

--- src/modules/identity/identity.controller.ts
+++ src/modules/identity/identity.controller.ts
@@ -1,12 +1,16 @@
 import { Public } from "@/infra/auth/decorators/public-route";
-import { BasicExceptionPresenter } from "@/infra/http/exceptions/basic.presenter";
 import exceptionsFactory from "@/infra/http/exceptions/exceptions-factory";
+import { ValidationErrorBagPresenter } from "@/infra/http/exceptions/validation/presenter";
 import { SignUpDto } from "@/modules/identity/dtos/signUp.dto";
 import { Account } from "@/modules/identity/entities/account.aggregate";
 import { AccountWithEmailAlreadyExistError } from "@/modules/identity/errors/account-with-email-already-exist.error";
 import { CreateAccountService } from "@/modules/identity/services/create-account.service";
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
-import { ApiConflictResponse, ApiOkResponse, ApiUnauthorizedResponse } from "@nestjs/swagger";
+import {
+  ApiConflictResponse,
+  ApiOkResponse,
+  ApiUnprocessableEntityResponse,
+} from "@nestjs/swagger";
 import { taskEither as te } from "fp-ts";
 import { pipe } from "fp-ts/lib/function";
 
@@ -24,9 +28,9 @@ export class IdentityController {
     type: AccountWithEmailAlreadyExistError,
     description: "Email already in use.",
   })
-  @ApiUnauthorizedResponse({
-    type: BasicExceptionPresenter,
-    description: "Provided credentials are wrong.",
+  @ApiUnprocessableEntityResponse({
+    type: ValidationErrorBagPresenter,
+    description: "Some of the inputs contain validation errors.",
   })
   @HttpCode(HttpStatus.NO_CONTENT)
   public createAccount(@Body() signUpDto: SignUpDto) {
