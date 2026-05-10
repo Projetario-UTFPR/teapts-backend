c281e287002c637f486014845c2f69d56354602c
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-10 20:26:16 -0300

    change: rename `TherapeuticJourney` to `ProjetoTerapeuticoSingular` (in portuguese yes) & add missing properties - Added `patientId` - Added `socialSituation` - Added `timeline` - Added boolean getters regarding PTS current status - Added constructors

--- src/modules/therapeutic-journey/aggregates/therapeutic-journey.entity.ts
+++
@@ -1,9 +0,0 @@
-import { AggregateRoot } from "@/common/entities/aggregate-root";
-import { type UUID } from "@/common/uuid";
-
-type TherapeuticJourneyProps = {
-  responsibleProfessionalId: UUID;
-  multidisciplinaryTeamIds: UUID[];
-};
-
-export class TherapeuticJourney extends AggregateRoot<TherapeuticJourneyProps> {}
