a04ab0b18b5f65531b66114c4aec695ee144dac1
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-16 02:50:40 -0300

    feat: add prisma PTS mapper

---
+++ src/infra/prisma/mappers/pts.mapper.ts
@@ -0,0 +1,79 @@
+import { ProjetoTerapeuticoSingular } from "@/modules/therapeutic-journey/aggregates/pts.aggregate";
+import { PtsTimeline } from "@/modules/therapeutic-journey/value-objects/pts-timeline.vo";
+import { $Enums, Prisma } from "@prisma-gen/browser";
+
+function statusIntoPrisma(PtsStatus: PtsTimeline.Status) {
+  switch (PtsStatus) {
+    case PtsTimeline.Status.Cancelled:
+      return $Enums.PtsStatus.Cancelled;
+    case PtsTimeline.Status.Concluded:
+      return $Enums.PtsStatus.Concluded;
+    case PtsTimeline.Status.Draft:
+      return $Enums.PtsStatus.Draft;
+    case PtsTimeline.Status.Planning:
+      return $Enums.PtsStatus.Planning;
+    case PtsTimeline.Status.Rejected:
+      return $Enums.PtsStatus.Rejected;
+    case PtsTimeline.Status.Running:
+      return $Enums.PtsStatus.Running;
+  }
+}
+
+function statusFromPrisma(PtsStatus: $Enums.PtsStatus) {
+  switch (PtsStatus) {
+    case $Enums.PtsStatus.Cancelled:
+      return PtsTimeline.Status.Cancelled;
+    case $Enums.PtsStatus.Concluded:
+      return PtsTimeline.Status.Concluded;
+    case $Enums.PtsStatus.Draft:
+      return PtsTimeline.Status.Draft;
+    case $Enums.PtsStatus.Planning:
+      return PtsTimeline.Status.Planning;
+    case $Enums.PtsStatus.Rejected:
+      return PtsTimeline.Status.Rejected;
+    case $Enums.PtsStatus.Running:
+      return PtsTimeline.Status.Running;
+  }
+}
+
+function intoPrisma(
+  pts: ProjetoTerapeuticoSingular,
+): Prisma.ProjetoTerapeuticoSingularCreateArgs["data"] {
+  const snapshot = pts.toSnapshot();
+  return {
+    socialSituation: snapshot.socialSituation,
+    status: statusIntoPrisma(snapshot.timeline.status),
+    acceptedAt: snapshot.timeline.acceptedAt,
+    beganAt: snapshot.timeline.beganAt,
+    cancelledAt: snapshot.timeline.cancelledAt,
+    concludedAt: snapshot.timeline.concludedAt,
+    createdAt: snapshot.timeline.createdAt,
+    rejectedAt: snapshot.timeline.rejectedAt,
+    id: snapshot.id.toString(),
+    patientId: snapshot.patientId.toString(),
+    responsibleProfessionalId: snapshot.responsibleProfessionalId.toString(),
+  };
+}
+
+function fromPrisma(
+  raw: Prisma.ProjetoTerapeuticoSingularModel & { multidisciplinaryTeamIds: string[] },
+): ProjetoTerapeuticoSingular {
+  return ProjetoTerapeuticoSingular.createUnchecked({
+    id: raw.id,
+    multidisciplinaryTeamIds: raw.multidisciplinaryTeamIds,
+    patientId: raw.patientId,
+    responsibleProfessionalId: raw.responsibleProfessionalId,
+    socialSituation: raw.socialSituation,
+    timeline: PtsTimeline.createUnchecked({
+      createdAt: raw.createdAt,
+      status: statusFromPrisma(raw.status),
+      acceptedAt: raw.acceptedAt ?? undefined,
+      beganAt: raw.beganAt ?? undefined,
+      cancelledAt: raw.cancelledAt ?? undefined,
+      concludedAt: raw.concludedAt ?? undefined,
+      rejectedAt: raw.rejectedAt ?? undefined,
+    }),
+  });
+}
+
+export default { fromPrisma, intoPrisma };
