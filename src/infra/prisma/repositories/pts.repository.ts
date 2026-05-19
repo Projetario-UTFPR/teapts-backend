15a16e7ba60fafa2f6d1ab231509fd69ec763baf
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-19 05:40:58 -0300

    feat: implement `PrismaPtsRepository`

--- src/infra/prisma/repositories/pts.repository.ts
+++ src/infra/prisma/repositories/pts.repository.ts
@@ -1,10 +1,13 @@
 import { IrrecoverableError } from "@/common/errors/irrecoverable.error";
 import { UUID } from "@/common/uuid";
+import ptsMapper from "@/infra/prisma/mappers/pts.mapper";
 import { PrismaService } from "@/infra/prisma/prisma";
 import { ProjetoTerapeuticoSingular } from "@/modules/therapeutic-journey/aggregates/pts.aggregate";
 import { PtsRepository } from "@/modules/therapeutic-journey/repositories/pts.repository";
 import { Injectable } from "@nestjs/common";
+import { taskEither as te } from "fp-ts";
 import { Either } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
 
 @Injectable()
 export class PrismaPtsRepository extends PtsRepository {
@@ -12,13 +15,46 @@ export class PrismaPtsRepository extends PtsRepository {
     super();
   }
 
+  // TODO: add integration tests to this method
   public activePtsExistsByPatientId(patientId: UUID): Promise<Either<IrrecoverableError, boolean>> {
-    throw new Error("Method not implemented.");
+    return pipe(
+      te.tryCatch(
+        () =>
+          this.prisma.projetoTerapeuticoSingular.count({
+            where: {
+              patientId: patientId.toString(),
+              AND: { status: { in: ["Running", "Planning"] } },
+            },
+          }),
+        (error) =>
+          new IrrecoverableError({
+            message: `Error occurred in ${PrismaPtsRepository.name} when trying to count active PTS from patient of ID '${patientId}'.`,
+            cause: error as Error,
+          }),
+      ),
+      te.map((count) => count !== 0),
+    )();
   }
 
   public createNewPts(
     pts: ProjetoTerapeuticoSingular,
   ): Promise<Either<IrrecoverableError, ProjetoTerapeuticoSingular>> {
-    throw new Error("Method not implemented.");
+    const payload = ptsMapper.intoPrisma(pts);
+
+    return pipe(
+      te.tryCatch(
+        () =>
+          this.prisma.projetoTerapeuticoSingular.create({
+            data: payload,
+            include: { multidisciplinaryTeam: { select: { professionalId: true } } },
+          }),
+        (error) =>
+          new IrrecoverableError({
+            message: `Error occurred in ${PrismaPtsRepository.name} when creating the PTS '${JSON.stringify(payload)}'.`,
+            cause: error as Error,
+          }),
+      ),
+      te.map(ptsMapper.fromPrisma),
+    )();
   }
 }
