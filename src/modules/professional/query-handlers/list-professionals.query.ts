07ba162f5eb4cbaf9f8b3a9f70fbefe76606631d
Kaio Felps <kaiofelipe2005@gmail.com>
2026-05-23 18:22:53 -0300

    feat: add `ListProfessionalsQueryHandler`

---
+++ src/modules/professional/query-handlers/list-professionals.query.ts
@@ -0,0 +1,103 @@
+import { IrrecoverableError } from "@/common/errors/irrecoverable.error";
+import { PaginationParams } from "@/common/pagination/pagination-params";
+import { PaginationResult } from "@/common/pagination/pagination-result";
+import { Professional } from "@/modules/professional/entities/professional.aggregate";
+import { ProfessionalWithAccountPresenter } from "@/modules/professional/presenters/professional-with-account.presenter";
+import { Either } from "fp-ts/lib/Either";
+import paginationUtlis from "@/common/pagination/pagination-utils";
+import professionalsMapper from "@/infra/prisma/mappers/professionals.mapper";
+import { PrismaService } from "@/infra/prisma/prisma";
+import { Injectable } from "@nestjs/common";
+import { ProfessionalFindManyArgs } from "@prisma-gen/models";
+import { taskEither as te } from "fp-ts";
+import { pipe } from "fp-ts/lib/function";
+
+export type Params = PaginationParams & {
+  specialisms?: Professional.Specialism[];
+  name?: string;
+};
+
+export type Result = PaginationResult & {
+  professionals: ProfessionalWithAccountPresenter[];
+};
+
+@Injectable()
+export class ListProfessionalsQueryHandler {
+  public constructor(private readonly prisma: PrismaService) {}
+
+  public execute({
+    limit,
+    page,
+    name,
+    specialisms,
+  }: Params): Promise<Either<IrrecoverableError, Result>> {
+    const { offset, resolvedPage, resolvedLimit } = paginationUtlis.resolveOffset({ page, limit });
+    const where = this.resolveWhereClause({ specialisms, name });
+    return pipe(
+      te.Do,
+      te.apS("professionals", this.fetchProfessionals({ where, limit, offset })),
+      te.apS("count", this.countProfessionals({ where })),
+      te.map(
+        ({ count, professionals }) =>
+          ({
+            professionals: professionals.map(ProfessionalWithAccountPresenter.present),
+            count: count,
+            currentPage: resolvedPage,
+            resolvedLimit,
+          }) satisfies Result,
+      ),
+    )();
+  }
+
+  private countProfessionals({ where }: { where: ProfessionalFindManyArgs["where"] }) {
+    return pipe(
+      te.tryCatch(
+        () => this.prisma.professional.count({ where }),
+        (error) =>
+          new IrrecoverableError({
+            message: `Unexpected error occurred in ${ListProfessionalsQueryHandler.name} when counting professionals.`,
+            cause: error as Error,
+          }),
+      ),
+    );
+  }
+
+  private fetchProfessionals({
+    limit,
+    where,
+    offset,
+  }: Pick<Params, "limit"> & {
+    where: ProfessionalFindManyArgs["where"];
+    offset: number;
+  }) {
+    return pipe(
+      te.tryCatch(
+        () =>
+          this.prisma.professional.findMany({
+            where,
+            take: limit,
+            skip: offset,
+            include: { account: true },
+            orderBy: [{ account: { name: "asc" } }, { id: "asc" }],
+          }),
+        (error) =>
+          new IrrecoverableError({
+            message: `Unexpected error occurred in ${ListProfessionalsQueryHandler.name} when fetching professionals.`,
+            cause: error as Error,
+          }),
+      ),
+    );
+  }
+
+  private resolveWhereClause({ specialisms, name }: Pick<Params, "name" | "specialisms">) {
+    let where: ProfessionalFindManyArgs["where"] = {};
+
+    if (name) where.account = { name: { contains: name, mode: "insensitive" } };
+
+    if (specialisms) {
+      where.specialism = { in: specialisms.map(professionalsMapper.specialismIntoPrisma) };
+    }
+
+    return where;
+  }
+}
