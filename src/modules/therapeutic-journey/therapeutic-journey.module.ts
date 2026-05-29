454ca2afd624138cfdd8660ddf561f51b0802b38
Marco4325 <markito1611@gmail.com>
2026-05-30 04:18:57 +0800

    feat(update-multidisciplinary-team): exporta módulo

--- src/modules/therapeutic-journey/therapeutic-journey.module.ts
+++ src/modules/therapeutic-journey/therapeutic-journey.module.ts
@@ -1,9 +1,10 @@
 import { PtsController } from "@/modules/therapeutic-journey/controllers/pts.controller";
 import { CreateDraftPtsService } from "@/modules/therapeutic-journey/services/create-draft-pts.service";
+import { UpdateMultidisciplinaryTeamService } from "@/modules/therapeutic-journey/services/update-multidisciplinary-team.service";
 import { Module } from "@nestjs/common";
 
 @Module({
   controllers: [PtsController],
-  providers: [CreateDraftPtsService],
+  providers: [CreateDraftPtsService, UpdateMultidisciplinaryTeamService],
 })
 export class TherapeuticJourneyModule {}
