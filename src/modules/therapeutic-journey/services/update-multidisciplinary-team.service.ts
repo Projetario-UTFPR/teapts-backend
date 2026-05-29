7bbbbc75f61cb0cfa0c4c145d320743ecdaad56b
Marco4325 <markito1611@gmail.com>
2026-05-30 04:15:52 +0800

    refact(update-multidisciplinary-team): refatora serviço conforme nova funcionalidade de retornar PTS por id

--- src/modules/therapeutic-journey/services/update-multidisciplinary-team.service.ts
+++ src/modules/therapeutic-journey/services/update-multidisciplinary-team.service.ts
@@ -1,31 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import { PtsRepository } from "@/modules/therapeutic-journey/repositories/pts.repository";
-import { ProjetoTerapeuticoSingular } from "@/modules/therapeutic-journey/aggregates/pts.aggregate";
-import { ProfessionalsRepository } from "@/modules/professional/professionals.repository";
 import type { UUID } from "@/common/uuid";
 import { ProfessionalIsNotResponsible } from "@/modules/professional/errors/professional-is-not-responsible.error";
 import { OneResponsibleIsNeeded } from "@/modules/professional/errors/one-responsible-is-needed.error";
 
 type Params = {
-  pts: ProjetoTerapeuticoSingular;
+  ptsId: UUID;
   professionalId: UUID;
   newResponsibleId?: UUID;
   multidisciplinaryTeamIds: UUID[];
 };
 
 @Injectable()
-export class UpdateMultidisciplinaryTeam {
-  public constructor(
-    private readonly ptsRepo: PtsRepository,
-    private readonly professionalsRepo: ProfessionalsRepository,
-  ) {}
+export class UpdateMultidisciplinaryTeamService {
+  public constructor(private readonly ptsRepo: PtsRepository) {}
 
   public async execute({
-    pts,
+    ptsId,
     professionalId,
     newResponsibleId,
     multidisciplinaryTeamIds,
   }: Params) {
+    const pts = await this.ptsRepo.getPtsById(ptsId);
+
     if (!pts.isResponsabilityOfProfessional(professionalId)) {
       throw new ProfessionalIsNotResponsible();
     }
