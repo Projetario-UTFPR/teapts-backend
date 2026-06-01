d9cc2e66664a9fc524961067c58392ff7556720e
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-01 01:38:56 -0300

    feat(`PtsRepository`): add `findActivePtsByPatientId` method (and `PtsNotFoundError`)

--- src/modules/therapeutic-journey/repositories/pts.repository.ts
+++ src/modules/therapeutic-journey/repositories/pts.repository.ts
@@ -2,6 +2,7 @@ import { IrrecoverableError } from "@/common/errors/irrecoverable.error";
 import { type UUID } from "@/common/uuid";
 import { ProjetoTerapeuticoSingular } from "@/modules/therapeutic-journey/aggregates/pts.aggregate";
 import { ProfessionalIsNotRegistered } from "@/modules/therapeutic-journey/errors/professional-is-not-registered.error";
+import { PtsNotFoundError } from "@/modules/therapeutic-journey/errors/pts-not-found.error";
 import { Either } from "fp-ts/lib/Either";
 
 export abstract class PtsRepository {
@@ -13,6 +14,15 @@ export abstract class PtsRepository {
   public abstract activePtsExistsByPatientId(
     patientId: UUID,
   ): Promise<Either<IrrecoverableError, boolean>>;
+
+  /**
+   * Finds the active PTS of patient identified by `patientId`.
+   *
+   * @note an 'active' PTS is a PTS that has either `Running` or `Planning` state.
+   */
+  public abstract findActivePtsByPatientId(
+    patientId: UUID,
+  ): Promise<Either<IrrecoverableError | PtsNotFoundError, ProjetoTerapeuticoSingular>>;
 
   /**
    * Saves the newly created `pts`. Shall not create it successfully if any of the professionals
