57861f5b21e2296f5e716c4de28bd5525acf1a02
Marco4325 <markito1611@gmail.com>
2026-06-07 12:20:54 -0300

    change(update-multidisciplinary-team): altera arquivos para corresponder corretamente ao esperado

--- src/modules/professional/errors/one-responsible-is-needed.error.ts
+++
@@ -1,9 +0,0 @@
-import { BadRequestError } from "@/common/errors/bad-request.error";
-
-export class OneResponsibleIsNeeded extends BadRequestError {
-  public constructor() {
-    super({
-      message: `É obrigatório ao menos um profissional responsável.`,
-    });
-  }
-}
