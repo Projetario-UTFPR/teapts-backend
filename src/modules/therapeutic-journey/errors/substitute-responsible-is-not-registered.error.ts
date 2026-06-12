2b6aa2c7b9f3fedd7b70405b0803b1f567a1e1a5
Marco4325 <markito1611@gmail.com>
2026-06-12 13:40:20 -0300

    delete(update-multidisciplinary-team): deleta erro redundante

--- src/modules/therapeutic-journey/errors/substitute-responsible-is-not-registered.error.ts
+++
@@ -1,9 +0,0 @@
-import { BadRequestError } from "@/common/errors/bad-request.error";
-
-export class SubstituteResponsibleIsNotRegistered extends BadRequestError {
-  public constructor() {
-    super({
-      message: "O ID do profissional substituto indicado não está registrado.",
-    });
-  }
-}
