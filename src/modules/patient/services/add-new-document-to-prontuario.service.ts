baf22d23d3016bc61a14d2225268a8d3f09fa931
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-13 20:16:40 -0300

    change(`AddNewDocumentToProntuarioService`): require the `assigneeProfessionalId`'s account to ensure the chosen profile belongs to the authenticated user

--- src/modules/patient/services/add-new-document-to-prontuario.service.ts
+++ src/modules/patient/services/add-new-document-to-prontuario.service.ts
@@ -1,4 +1,5 @@
 import { type UUID } from "@/common/uuid";
+import { Account } from "@/modules/identity/entities/account.aggregate";
 import { Document } from "@/modules/patient/aggregates/document.aggregate";
 import { DocumentsRepository } from "@/modules/patient/repositories/documents.repository";
 import { DocumentFilesStorage } from "@/modules/patient/storage/document-files.storage";
@@ -9,6 +10,7 @@ import { pipe } from "fp-ts/lib/function";
 
 type Params = {
   patientId: UUID;
+  account: Account;
   assigneeProfessionalId: UUID;
   documentFileKey: string;
   documentTitle: string;
@@ -36,11 +38,13 @@ export class AddNewDocumentToProntuarioService {
     documentTitle,
     documentDescription,
     documentFileKey,
+    account,
   }: Params) {
     return pipe(
       () =>
         this.verifyProfessionalIsAuthorized.execute({
           patientId,
+          account,
           professionalId: assigneeProfessionalId,
         }),
       te.chainW(() =>
