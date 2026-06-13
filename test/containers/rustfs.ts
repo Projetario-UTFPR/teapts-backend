9d617a4c0a80562e7da8f1eccb56e72b9659b6ba
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-13 15:49:11 -0300

    test: add `.env.test` file with non-sensitive environment variables for test suits and testcontainers to be raised

--- test/containers/rustfs.ts
+++ test/containers/rustfs.ts
@@ -1,30 +1,35 @@
-import { GenericContainer } from "testcontainers";
+import { GenericContainer, Wait } from "testcontainers";
 
 export async function getRustfsContainer(testId: string) {
   process.env.BLOB_STORAGE_ACCESS_KEY ??= "test_rustfs";
   process.env.BLOB_STORAGE_SECRET_KEY ??= "test_rustfs";
 
-  const rustfsContainer = await new GenericContainer("rustfs/rustfs:latest")
-    .withExposedPorts(9000) // container's internal exposed port
-    .withEnvironment({
-      RUSTFS_ACCESS_KEY: `${process.env.BLOB_STORAGE_ACCESS_KEY}`,
-      RUSTFS_SECRET_KEY: `${process.env.BLOB_STORAGE_SECRET_KEY}`,
-      RUSTFS_CONSOLE_ENABLE: `${process.env.BLOB_STORAGE_CONSOLE_ENABLE}`,
-      RUSTFS_SERVER_DOMAINS: `${process.env.BLOB_STORAGE_SERVER_DOMAINS}`,
-    })
-    .start();
+  try {
+    const rustfsContainer = await new GenericContainer("rustfs/rustfs:latest")
+      .withExposedPorts(9000) // container's internal exposed port
+      .withEnvironment({
+        RUSTFS_ACCESS_KEY: `${process.env.BLOB_STORAGE_ACCESS_KEY}`,
+        RUSTFS_SECRET_KEY: `${process.env.BLOB_STORAGE_SECRET_KEY}`,
+        RUSTFS_CONSOLE_ENABLE: "false",
+        RUSTFS_SERVER_DOMAINS: "localhost",
+      })
+      .withWaitStrategy(Wait.forListeningPorts())
+      .start();
 
-  const port = rustfsContainer.getFirstMappedPort().toString();
+    const port = rustfsContainer.getFirstMappedPort().toString();
 
-  process.env.BLOB_STORAGE_PORT = port;
-  process.env.BLOB_STORAGE_URL = `http://127.0.0.1:${port}`;
+    process.env.BLOB_STORAGE_PORT = port;
+    process.env.BLOB_STORAGE_URL = `http://127.0.0.1:${port}`;
 
-  process.env.DOCUMENTS_BUCKET = (process.env.DOCUMENTS_BUCKET ?? "documents") + "-" + testId;
+    process.env.DOCUMENTS_BUCKET = (process.env.DOCUMENTS_BUCKET ?? "documents") + "-" + testId;
 
-  process.env.PROFILE_PICTURES_BUCKET =
-    (process.env.PROFILE_PICTURES_BUCKET ?? "profilespics") + "-" + testId;
+    process.env.PROFILE_PICTURES_BUCKET =
+      (process.env.PROFILE_PICTURES_BUCKET ?? "profilespics") + "-" + testId;
 
-  process.env.BLOB_STORAGE_REGION = "us-east-1";
+    process.env.BLOB_STORAGE_REGION = "us-east-1";
 
-  return rustfsContainer;
+    return rustfsContainer;
+  } catch (error) {
+    console.error("Failed to start RustFS container:", error);
+  }
 }
