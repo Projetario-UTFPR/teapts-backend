9b97c933e5fb7f528597d4ec333db3f5c6ae9334
Marco4325 <markito1611@gmail.com>
2026-06-19 15:18:20 -0300

    feat(create-activity): add DocumentNotFoundError

---
+++ src/modules/patient/errors/document-not-found-error.ts
@@ -0,0 +1,9 @@
+import { ResourceNotFoundError } from "@/common/errors/resource-not-found.error";
+import { UUID } from "@/common/uuid";
+import { Document } from "../aggregates/document.aggregate";
+
+export class DocumentNotFoundError extends ResourceNotFoundError {
+    public constructor(id: UUID) {
+        super({ message: `O documento especificado não foi encontrado. ID: ${id}`, subject: Document.name });
+    }
+}
