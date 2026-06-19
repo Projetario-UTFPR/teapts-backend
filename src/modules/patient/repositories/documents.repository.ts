13906f267c8bbd94577b6959532e73cf1286109c
Marco4325 <markito1611@gmail.com>
2026-06-19 15:25:32 -0300

    feat(create-activity): add getById to document repository interface

--- src/modules/patient/repositories/documents.repository.ts
+++ src/modules/patient/repositories/documents.repository.ts
@@ -4,6 +4,8 @@ import { PatientNotFoundError } from "@/modules/patient/errors/patient-not-found
 // oxlint-disable-next-line no-unused-vars We need to import it so that we can link to it in the docstrings
 import { type DocumentFilesStorage } from "@/modules/patient/storage/document-files.storage";
 import { type Either } from "fp-ts/lib/Either";
+import { DocumentNotFoundError } from "../errors/document-not-found-error";
+import { UUID } from "@/common/uuid";
 
 /**
  * This repository manages the entity `Document`, not the descriptor itself.
@@ -14,4 +16,6 @@ export abstract class DocumentsRepository {
   public abstract createDocument(
     document: Document,
   ): Promise<Either<IrrecoverableError | PatientNotFoundError, Document>>;
+
+  public abstract getById(id: UUID): Promise<Either<IrrecoverableError | DocumentNotFoundError, Document>>;
 }
