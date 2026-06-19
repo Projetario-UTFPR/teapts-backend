769c9b0c22241b1d169890ba0bcd1d6d126083c8
Marco4325 <markito1611@gmail.com>
2026-06-19 15:25:11 -0300

    feat(create-activity): add getById to in memory document repository

--- test/mocks/repositories/in-memory/documents.repository.ts
+++ test/mocks/repositories/in-memory/documents.repository.ts
@@ -1,7 +1,9 @@
 import { IrrecoverableError } from "@/common/errors/irrecoverable.error";
+import { UUID } from "@/common/uuid";
 import { Document } from "@/modules/patient/aggregates/document.aggregate";
+import { DocumentNotFoundError } from "@/modules/patient/errors/document-not-found-error";
 import { DocumentsRepository } from "@/modules/patient/repositories/documents.repository";
-import { either } from "fp-ts";
+import { either as e } from "fp-ts";
 import { Either } from "fp-ts/lib/Either";
 
 export class InMemoryDocumentsRepository implements DocumentsRepository {
@@ -9,6 +11,15 @@ export class InMemoryDocumentsRepository implements DocumentsRepository {
 
   public async createDocument(document: Document): Promise<Either<IrrecoverableError, Document>> {
     this.items.push(document);
-    return either.right(document);
+    return e.right(document);
+  }
+
+  public async getById(id: UUID): Promise<Either<IrrecoverableError | DocumentNotFoundError, Document>> {
+    const document = this.items.find((doc) => doc.getId() === id);
+
+    if (!document) {
+      return e.left(new DocumentNotFoundError(id));
+    }
+    return e.right(document);
   }
 }
