13a6fceb667fa04c3b81f8a3c0cf12d233a8a0d3
Marco4325 <markito1611@gmail.com>
2026-06-19 15:18:41 -0300

    feat(create-activity): add DocumentDoesNotBelongToPatientError

---
+++ src/modules/patient/errors/document-does-not-belong-to-patient.error.ts
@@ -0,0 +1,16 @@
+import { BadRequestError } from "@/common/errors/bad-request.error";
+import { UUID } from "@/common/uuid";
+
+/**
+ * Indicates that a professional tried to activate a document's file, but
+ * the document hasn't been stored yet.
+ */
+export class DocumentDoestNotBelongToPatientError extends BadRequestError {
+    public constructor(id: UUID) {
+        super({
+            message:
+                `O documento identificado por "${id}" não pertence ao paciente provido, ` +
+                "e portanto não pode ser vinculado.",
+        });
+    }
+}
