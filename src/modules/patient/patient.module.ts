587be54db164f60a755f99db9b2143dfcca02a84
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-23 04:28:55 -0300

    feat: add `ListProntuarioByPatientIdQueryHandler`

--- src/modules/patient/patient.module.ts
+++ src/modules/patient/patient.module.ts
@@ -1,4 +1,5 @@
 import { ProntuarioController } from "@/modules/patient/controllers/prontuario.controller";
+import { ListProntuarioByPatientIdQueryHandler } from "@/modules/patient/query-handlers/list-prontuario-by-patient-id.query";
 import { AddNewDocumentToProntuarioService } from "@/modules/patient/services/add-new-document-to-prontuario.service";
 import { SignDocumentUploadUrlService } from "@/modules/patient/services/sign-document-upload-url.service";
 import { TherapeuticJourneyModule } from "@/modules/therapeutic-journey/therapeutic-journey.module";
@@ -8,7 +9,11 @@ import { Module } from "@nestjs/common";
   // when it comes that the therapeutic journey module also needs to import this module,
   // use `forwardRef(() => module)` in both of them to solve this cyclic import issue.
   imports: [TherapeuticJourneyModule],
-  providers: [SignDocumentUploadUrlService, AddNewDocumentToProntuarioService],
+  providers: [
+    SignDocumentUploadUrlService,
+    AddNewDocumentToProntuarioService,
+    ListProntuarioByPatientIdQueryHandler,
+  ],
   controllers: [ProntuarioController],
 })
 export class PatientModule {}
