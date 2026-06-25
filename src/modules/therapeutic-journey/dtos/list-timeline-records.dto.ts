9726633aae8eb1f014c44b6c4b6235747285ff35
Marco4325 <markito1611@gmail.com>
2026-06-25 11:58:18 -0300

    feat(list-timeline-records): adds DTO for listing timeline-records

---
+++ src/modules/therapeutic-journey/dtos/list-timeline-records.dto.ts
@@ -0,0 +1,62 @@
+import { BasePaginationDto } from "@/common/pagination/pagination-dto";
+import { TimelineRecord } from "@/modules/therapeutic-journey/aggregates/timeline-record.aggregate";
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { Expose } from "class-transformer";
+import z from "zod";
+
+const schema = z
+    .object({
+        professionalId: z
+            .uuid("O ID do profissional precisa ser um UUID válido.")
+            .optional(),
+        target: z
+            .enum(TimelineRecord.TargetType, {
+                error: "O alvo (target) fornecido para o filtro é inválido.",
+            })
+            .optional(),
+        type: z
+            .enum(TimelineRecord.Type, {
+                error: "O tipo (type) de evento fornecido para o filtro é inválido.",
+            })
+            .optional(),
+        description: z
+            .string()
+            .trim()
+            .optional(),
+    })
+    .extend(BasePaginationDto.baseSchema.shape);
+
+type ListTimelineRecordsSchema = z.infer<typeof schema>;
+
+export class ListTimelineRecordsDto extends BasePaginationDto implements ListTimelineRecordsSchema {
+    @Expose()
+    @ApiPropertyOptional({
+        description: "Filters the timeline records by a specific professional profile ID.",
+        type: "string",
+        format: "uuid",
+    })
+    public readonly professionalId?: string | undefined;
+
+    @Expose()
+    @ApiPropertyOptional({
+        description: "Filters the timeline records by a specific target (e.g., pts, activity).",
+        enum: TimelineRecord.TargetType,
+    })
+    public readonly target?: TimelineRecord.TargetType | undefined;
+
+    @Expose()
+    @ApiPropertyOptional({
+        description: "Filters the timeline records by the nature of the event (e.g., created, approved).",
+        enum: TimelineRecord.Type,
+    })
+    public readonly type?: TimelineRecord.Type | undefined;
+
+    @Expose()
+    @ApiPropertyOptional({
+        description: "Filters the timeline records by text search in the description.",
+        type: "string",
+    })
+    public readonly description?: string | undefined;
+
+    protected schema = schema;
+}
