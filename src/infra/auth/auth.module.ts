b4d8d04c86553c01bf236ccbf2fb368eaa5ef2d4
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-28 20:40:23 -0300

    feat: add `RefreshTokenService`

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -11,6 +11,7 @@ import { PassportModule } from "@nestjs/passport";
 import { APP_GUARD } from "@nestjs/core";
 import { JwtAuthGuard } from "@/infra/auth/jwt/guard";
 import { GetAuthCollectionQueryHandler } from "@/infra/auth/query-handlers/get-auth-collection.query";
+import { RefreshTokenService } from "@/infra/auth/services/refresh-token.service";
 
 @Global()
 @Module({
@@ -35,7 +36,8 @@ import { GetAuthCollectionQueryHandler } from "@/infra/auth/query-handlers/get-a
     JwtStrategy,
     { provide: APP_GUARD, useClass: JwtAuthGuard },
     GetAuthCollectionQueryHandler,
+    RefreshTokenService,
   ],
-  exports: [AssignTokenService, GetAuthCollectionQueryHandler],
+  exports: [AssignTokenService, GetAuthCollectionQueryHandler, RefreshTokenService],
 })
 export class AuthModule {}
